17470a295b7c667ad08974a64dbbc001f8312a55
Vania_Dmitriienko <dmitrienko2706@gmail.com>
2022-12-13 19:46:35 +0200

    add API

---
+++ src/service/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const API_KEY = 'f530f3a1ba7a2ce8b8185ab97973a95f';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+export const getTrendingMoviee = async () => {
+  console.log(getTrendingMoviee);
+  return await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
+};
+
+export const getMovieName = async query => {
+  return await axios.get(`/search/movie?api_key=${API_KEY}&query=${query}`);
+};
+
+export const getMovieId = async id => {
+  return await axios.get(`/movie/${id}?api_key=${API_KEY}`);
+};
+
+export const getCredits = async id => {
+  return await axios.get(`/movie/${id}/credits?api_key=${API_KEY}`);
+};
+
+export const getReviews = async id => {
+  return await axios.get(`/movie/${id}/reviews?api_key=${API_KEY}`);
+};
